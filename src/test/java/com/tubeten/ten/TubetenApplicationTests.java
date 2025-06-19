6de7bbb561499821e665dc1f7a672b470d81ffe4
dakcoh <dakcoh@gmail.com>
2025-06-19 11:37:07 +0900

    app_yml local, prod 분기 mysql dependencies 추가

--- src/test/java/com/tubeten/ten/TubetenApplicationTests.java
+++ src/test/java/com/tubeten/ten/TubetenApplicationTests.java
@@ -2,8 +2,10 @@ package com.tubeten.ten;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class TubetenApplicationTests {
 
 	@Test
